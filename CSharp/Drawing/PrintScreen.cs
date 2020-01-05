96ab1da2e646d202ba8dd314ffc882236c186412
Gabriel Scatolin <cyp.g@pm.me>
2020-01-04 23:23:54 -0300

    adicionado diretiva static

--- CSharp/Drawing/PrintScreen.cs
+++ CSharp/Drawing/PrintScreen.cs
@@ -1,5 +1,5 @@
-using System.Drawing.Imaging.ImageFormat; //Somente à partir do C# 6
-using System.Clipboard; //Somente à partir do C# 6, caso contrário, use apenas o namespace
+using static System.Drawing.Imaging.ImageFormat; //Somente à partir do C# 6
+using static System.Clipboard; //Somente à partir do C# 6, caso contrário, use apenas o namespace
 
 if (ContainsImage())  
     GetImage().Save(@"image.jpeg", Jpeg);
